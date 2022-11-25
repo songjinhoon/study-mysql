33daf6e8e584f9b677c468d26253de1fcce11f07
hijinhoon <hijinhoon@muhanit.kr>
2022-11-25 18:36:42 +0900

    Initial commit

---
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.fastcampusmysql.domain.member.entity;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+public class Member {
+
+/*    final private Long id;
+
+    private String nickname;
+
+    final private String email;
+
+    final private LocalDate birthday;
+
+    final private LocalDateTime created;*/
+
+}
---
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriterService.java
@@ -0,0 +1,14 @@
+package com.example.fastcampusmysql.domain.member.service;
+
+import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
+
+public class MemberWriterService {
+
+    /**
+     * 회원 등록
+     * */
+    public void create(RegisterMemberCommand registerMemberCommand) {
+
+    }
+
+}
